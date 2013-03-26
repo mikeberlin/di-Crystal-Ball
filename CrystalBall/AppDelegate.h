dc7dfcf7646747d3bc3c2fea796f400fc46ea438
Mike Berlin <mikeberlin@me.com>
2013-03-26 17:46:57 -0500

    Initial Commit

---
+++ CrystalBall/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CrystalBall
+//
+//  Created by Mike Berlin on 3/26/13.
+//  Copyright (c) 2013 Mike Berlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CrystalBall/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CrystalBall
+//
+//  Created by Mike Berlin on 3/26/13.
+//  Copyright (c) 2013 Mike Berlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
